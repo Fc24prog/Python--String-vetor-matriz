b624c45c9f46dd6462bb2832d907f71ba184157a
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:54:32 -0300

    Add files via upload
    #Criando uma matriz 3x4 inicializada com 0 usando
    #append - lembrando que o método append
    #acrescenta uma nova posição no vet

---
+++ teste 01.py
@@ -0,0 +1,12 @@
+mat=[]
+for i in range (0,3): #linha
+    mat.append(0)
+    mat[i]=[]
+    for j in range (0,4): # coluna
+        mat[i].append(0)
+print(mat)
+
+
+#Criando uma matriz 3x4 inicializada com 0 usando
+#append - lembrando que o método append
+#acrescenta uma nova posição no vet
