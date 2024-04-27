032a4c04b1a0285a260f4b255deedd88e4c995ce
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-27 10:32:24 -0300

    Add files via upload
    #Leia um vetor de 40 posições e conte quantos elementos
    #pares se encontram no vetor.

---
+++ 2.py
@@ -0,0 +1,21 @@
+vet=[]
+cont=0
+for i in range(0,4):
+    vet.append(input("Entre com os elementos: "))
+for i in range(0,4):
+    
+    if vet[i]%2==0:
+           cont=cont+1
+           print("a quantidade de pares no vetor são: ",cont)
+
+
+
+
+
+
+
+
+
+
+#Leia um vetor de 40 posições e conte quantos elementos
+#pares se encontram no vetor.
